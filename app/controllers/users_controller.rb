d00b2e037acd20a4b68c2ab345d3e9e45956ec91
Rmiverson <Rmiverson99@gmail.com>
2021-01-27 11:12:41 -0800

    added show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,9 @@ class UsersController < ApplicationController
         user = User.find_or_create_by(name: params['name'])
         render :json => user
     end
+
+    def show
+        user = User.find(params[:id])
+        render :json => user
+    end
 end
